d8aa3f7559a5ea9c3487e5ad8ef0e6849d1802ba
DursunAlperenCoban <dursunalperen28@gmail.com>
2021-04-10 11:05:07 +0300

    This update for index php again

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php echo "Welcome to the midterms";?>
+<form action="dac.php" method="post">
+<input type="text" placeholder="Licence Plate" name="licence">
+<input type="text" placeholder="Brand" name="brand">
+<input type="text" placeholder="Model" name="model">
+<input type="text" placeholder="ModelYear" name="year">
+<input type="text" placeholder="Color" name="color">
+<input type="submit" name ="Send">
+
+</form>
