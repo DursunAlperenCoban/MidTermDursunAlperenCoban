d90a49b627fee55585c4f346e1f97e1f40ff5493
DursunAlperenCoban <dursunalperen28@gmail.com>
2021-04-10 11:00:34 +0300

    Added Automobile php file

---
+++ Automobile.php
@@ -0,0 +1,22 @@
+<?php
+class Automobile{
+    public $licencePlate;
+    public $brand;
+    public $model;
+    public $modelYear;
+    public $color;
+
+    function construct($licencePlate,$brand,$model,$modelYear,$color)
+    {
+        $this->licencePlate=$licencePlate;
+        $this->brand=$brand;
+        $this->model=$model;
+        $this->modelYear=$modelYear;
+        $this->color=$color;
+    }
+    function toString()
+    {
+      return  "Licence Plate: ".$this->licencePlate ."<br>Brand: ". $this->brand ."<br>Model: ". $this->model ."<br>Year: ". $this->modelYear ."<br>Color: ". $this->color;
+    }
+}
+?>
